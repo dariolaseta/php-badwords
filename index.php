01d10657937c0ca43c2a832fe91e3dbb90cac04b
Oxidra <dantelezzo@gmail.com>
2023-06-30 14:04:13 +0200

    created form

---
+++ index.php
@@ -0,0 +1,5 @@
+<form action="server.php">
+    <input type="text" name="text" placeholder="Inserisci una parola">
+    <input type="text" name="badword" placeholder="Inserisci una badword">
+    <button type="submit">Invia</button>
+</form>
